0caad6124669a984aef0f6a8590dc3e30c8c666d
Nibras Sameeh <nibras.s@aeturnum.com>
2025-02-22 17:55:20 +0530

    added maven spotless plugin

--- catalog-service/src/test/java/com/example/bookstore/catalog/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/com/example/bookstore/catalog/CatalogServiceApplicationTests.java
@@ -8,8 +8,6 @@ import org.springframework.context.annotation.Import;
 @Import(TestcontainersConfiguration.class)
 class CatalogServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
