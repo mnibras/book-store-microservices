a11f8c705d3f7c94934c03c95742add093342f1c
Nibras Sameeh <nibras.s@aeturnum.com>
2025-02-25 16:00:46 +0530

    added get products api

---
+++ catalog-service/src/main/java/com/example/bookstore/catalog/domain/Product.java
@@ -0,0 +1,62 @@
+package com.example.bookstore.catalog.domain;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private String code;
+    private String name;
+    private String description;
+    private String imageUrl;
+    private BigDecimal price;
+
+    public Product() {}
+
+    public Product(String code, String name, String description, String imageUrl, BigDecimal price) {
+        this.code = code;
+        this.name = name;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.price = price;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
---
+++ catalog-service/src/main/java/com/example/bookstore/catalog/domain/ProductMapper.java
@@ -0,0 +1,15 @@
+package com.example.bookstore.catalog.domain;
+
+class ProductMapper {
+
+    private ProductMapper() {}
+
+    static Product toProduct(ProductEntity productEntity) {
+        return new Product(
+                productEntity.getCode(),
+                productEntity.getName(),
+                productEntity.getDescription(),
+                productEntity.getImageUrl(),
+                productEntity.getPrice());
+    }
+}
