f60fd54321f1849128f95ed2fbe4b05ebc22315c
Nibras Sameeh <nibras.s@aeturnum.com>
2025-03-01 21:12:32 +0530

    updated ci pipeline

--- order-service/src/main/java/com/bookstore/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/bookstore/order/OrderServiceApplication.java
@@ -10,7 +10,6 @@ public class OrderServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
-        System.out.println();
     }
 
 }
