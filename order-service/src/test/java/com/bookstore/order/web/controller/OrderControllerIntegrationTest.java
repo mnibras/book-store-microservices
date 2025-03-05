180d8348e85532bf051174383a80a66b9f3bbda1
Nibras Sameeh <nibras.s@aeturnum.com>
2025-03-05 18:13:16 +0530

    added unit and integration test for order-service

---
+++ order-service/src/test/java/com/bookstore/order/web/controller/OrderControllerIntegrationTest.java
@@ -0,0 +1,69 @@
+package com.bookstore.order.web.controller;
+
+import com.bookstore.order.AbstractIT;
+import com.bookstore.order.testdata.TestDataFactory;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+class OrderControllerIntegrationTest extends AbstractIT {
+
+    @Nested
+    class CreateOrderTests {
+
+    }
+
+    @Test
+    void shouldCreateOrderSuccessfully() {
+        var payload =
+                """
+                            {
+                                "customer" : {
+                                    "name": "Nibras",
+                                    "email": "nibras@gmail.com",
+                                    "phone": "12356789"
+                                },
+                                "deliveryAddress" : {
+                                    "addressLine1": "Park Road",
+                                    "addressLine2": "Random Street",
+                                    "city": "New York",
+                                    "state": "NY",
+                                    "zipCode": "54613",
+                                    "country": "USA"
+                                },
+                                "items": [
+                                    {
+                                        "code": "P100",
+                                        "name": "Product 1",
+                                        "price": 25.50,
+                                        "quantity": 1
+                                    }
+                                ]
+                            }
+                        """;
+
+        given().contentType(ContentType.JSON)
+                .body(payload)
+                .when()
+                .post("/api/orders")
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .body("orderNumber", notNullValue());
+    }
+
+    @Test
+    void shouldReturnBadRequestWhenMandatoryDataIsMissing() {
+        var payload = TestDataFactory.createOrderRequestWithInvalidCustomer();
+        given().contentType(ContentType.JSON)
+                .body(payload)
+                .when()
+                .post("/api/orders")
+                .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value());
+    }
+
+}
