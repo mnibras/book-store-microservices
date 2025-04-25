b13d7d3bedd31f063087d5f6f7c4de7f6222ffd3
Nibras Sameeh <nibras.s@aeturnum.com>
2025-04-25 10:30:09 +0530

    integration tests added for order-service

--- order-service/src/test/java/com/bookstore/order/web/controller/OrderControllerIntegrationTest.java
+++ order-service/src/test/java/com/bookstore/order/web/controller/OrderControllerIntegrationTest.java
@@ -1,72 +1,99 @@
 package com.bookstore.order.web.controller;
 
 import com.bookstore.order.AbstractIT;
-import com.bookstore.order.testdata.TestDataFactory;
+import com.bookstore.order.domain.model.OrderSummary;
+import io.restassured.common.mapper.TypeRef;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
-class OrderControllerIntegrationTest extends AbstractIT {
+@Sql("/test-orders.sql")
+class OrderControllerTests extends AbstractIT {
 
     @Nested
     class CreateOrderTests {
 
+
+        @Test
+        void shouldCreateOrderSuccessfully() {
+            mockGetProductByCode("P100", "Product 1", new BigDecimal("25.50"));
+            var payload =
+                    """
+                                {
+                                    "customer" : {
+                                        "name": "Nibras",
+                                        "email": "nibras@gmail.com",
+                                        "phone": "12356789"
+                                    },
+                                    "deliveryAddress" : {
+                                        "addressLine1": "Park Road",
+                                        "addressLine2": "Random Street",
+                                        "city": "New York",
+                                        "state": "NY",
+                                        "zipCode": "54613",
+                                        "country": "USA"
+                                    },
+                                    "items": [
+                                        {
+                                            "code": "P100",
+                                            "name": "Product 1",
+                                            "price": 25.50,
+                                            "quantity": 1
+                                        }
+                                    ]
+                                }
+                            """;
+
+            given().contentType(ContentType.JSON)
+                    .body(payload)
+                    .when()
+                    .post("/api/orders")
+                    .then()
+                    .statusCode(HttpStatus.CREATED.value())
+                    .body("orderNumber", notNullValue());
+        }
     }
 
-    @Test
-    void shouldCreateOrderSuccessfully() {
-        mockGetProductByCode("P100", "Product 1", new BigDecimal("25.50"));
-        var payload =
-                """
-                            {
-                                "customer" : {
-                                    "name": "Nibras",
-                                    "email": "nibras@gmail.com",
-                                    "phone": "12356789"
-                                },
-                                "deliveryAddress" : {
-                                    "addressLine1": "Park Road",
-                                    "addressLine2": "Random Street",
-                                    "city": "New York",
-                                    "state": "NY",
-                                    "zipCode": "54613",
-                                    "country": "USA"
-                                },
-                                "items": [
-                                    {
-                                        "code": "P100",
-                                        "name": "Product 1",
-                                        "price": 25.50,
-                                        "quantity": 1
-                                    }
-                                ]
-                            }
-                        """;
+    @Nested
+    class GetOrdersTests {
 
-        given().contentType(ContentType.JSON)
-                .body(payload)
-                .when()
-                .post("/api/orders")
-                .then()
-                .statusCode(HttpStatus.CREATED.value())
-                .body("orderNumber", notNullValue());
+        @Test
+        void shouldGetOrdersSuccessfully() {
+            List<OrderSummary> orderSummaries = given().when()
+                    .get("/api/orders")
+                    .then()
+                    .statusCode(200)
+                    .extract()
+                    .body()
+                    .as(new TypeRef<>() {
+                    });
+
+            assertThat(orderSummaries).hasSize(2);
+        }
     }
 
-    @Test
-    void shouldReturnBadRequestWhenMandatoryDataIsMissing() {
-        var payload = TestDataFactory.createOrderRequestWithInvalidCustomer();
-        given().contentType(ContentType.JSON)
-                .body(payload)
-                .when()
-                .post("/api/orders")
-                .then()
-                .statusCode(HttpStatus.BAD_REQUEST.value());
-    }
+    @Nested
+    class GetOrderByOrderNumberTests {
+        String orderNumber = "order-123";
 
+        @Test
+        void shouldGetOrderSuccessfully() {
+            given().when()
+                    .get("/api/orders/{orderNumber}", orderNumber)
+                    .then()
+                    .statusCode(200)
+                    .body("orderNumber", is(orderNumber))
+                    .body("items.size()", is(2));
+        }
+    }
 }
