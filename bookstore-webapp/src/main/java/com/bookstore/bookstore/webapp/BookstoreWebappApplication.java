90c91acb7122530e5b96f8b8943ada3ddc522550
Nibras Sameeh <nibras.s@aeturnum.com>
2025-05-09 17:45:32 +0530

    modified bookstore-webapp.yml

--- bookstore-webapp/src/main/java/com/bookstore/bookstore/webapp/BookstoreWebappApplication.java
+++ bookstore-webapp/src/main/java/com/bookstore/bookstore/webapp/BookstoreWebappApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 public class BookstoreWebappApplication {
 
 	public static void main(String[] args) {
-		System.out.println();
 		SpringApplication.run(BookstoreWebappApplication.class, args);
 	}
 
